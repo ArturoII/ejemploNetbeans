4da30676d9b85c6ba21aae2f6351ef2e37e64a1f
Juan Guarnizo <juandavidguarnizoc@gmail.com>
2023-09-21 01:52:40 -0500

    Se agrego la clase Estudiante

---
+++ src/test_git/Estudiante.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test_git;
+
+/**
+ * Esta Clase es una entidad Estudiante
+ * @author Usuario
+ */
+public class Estudiante {
+    
+}
